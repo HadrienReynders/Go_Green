470f2e6f7b15f07714bbd0fed578bdf35483f4d3
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-03-01 14:51:34 +0100

    Added minimum info on gardens pages and garden_users page

--- app/controllers/garden_users_controller.rb
+++ app/controllers/garden_users_controller.rb
@@ -1,2 +1,7 @@
 class GardenUsersController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def index
+    @garden_users = GardenUser.all
+  end
 end
