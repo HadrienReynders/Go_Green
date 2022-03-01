4c96895048b7679644220f416adafec0dec721e8
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-03-01 12:04:22 +0100

    All routes added + controllers and view files

---
+++ app/controllers/garden_users_controller.rb
@@ -0,0 +1,2 @@
+class GardenUsersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :gardens, only: [ :index ]
+  resources :gardens, only: [ :index ] do
+    resources :garden_users, only: [ :index, :show ]
+  end
+
+  resources :users do
+    resources :select_tasks, only: [ :index, :update ]
+  end
+
+  resources :tasks, only: [ :index ]
 
 end
