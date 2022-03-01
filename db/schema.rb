fab4c0d1d439e08cead2de7cc860f05f163dc34e
julien lozet <lozetjulien@gmail.com>
2022-03-01 11:33:32 +0100

    add avatar_urm column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_093817) do
+ActiveRecord::Schema.define(version: 2022_03_01_103018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_093817) do
     t.integer "exp"
     t.string "seed_kind"
     t.integer "status"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
