fbfeb917c9b8fcd5cf0f290d12193fb47f5d0761
Hadrien Reynders <reynders.hadrien@gmail.com>
2022-03-04 15:42:04 +0100

    fixed bean_6.png

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Notifications::Engine => "/notifications"
   devise_for :users
-  root to: 'sessions#new'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "dashboard", to: "pages#dashboard"
 
