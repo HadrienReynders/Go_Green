56b8f9787f6c94a6811a81232b574e5dbc1b2a8e
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-03-07 10:33:05 +0100

    Login button now directs to first existing garden for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || dashboard_path(garden_id: Garden.first.id)
+    stored_location_for(resource) || dashboard_path(garden_id: GardenUser.order(garden_id: :asc).find { |gardenuser| gardenuser.user_id == 2 }.garden_id)
   end
 end
