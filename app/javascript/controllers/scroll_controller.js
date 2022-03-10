59eddfdc261d3c87a10a61498d2ac34f4edb9f8c
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-03-10 15:28:45 +0100

    added scroll

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,5 @@
+import { Application } from 'stimulus'
+import { ScrollTo } from 'stimulus-scroll-to'
+
+const application = Application.start()
+application.register("scroll-to", ScrollTo)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,5 +12,5 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "controllers"
 import "bootstrap"
+import "controllers"
